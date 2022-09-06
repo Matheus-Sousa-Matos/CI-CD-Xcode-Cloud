8883234dba815eeeff471dcc0453b66220d81ffe
Matheus de Sousa Matos <69324938+Matheus-Sousa-Matos@users.noreply.github.com>
2022-09-06 17:09:30 -0300

    Add files via upload

---
+++ CI-CD-Xcode-Cloud/CI-CD-Xcode-Cloud/CI_CD_Xcode_CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CD_Xcode_CloudApp.swift
+//  CI-CD-Xcode-Cloud
+//
+//  Created by Matheus de Sousa Matos on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CI_CD_Xcode_CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
